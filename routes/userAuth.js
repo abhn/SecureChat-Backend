3bac5df8f89ab11b4b95127712d2de59ef5cfbd2
Abhishek Nagekar <abhisheknagekar@gmail.com>
2017-07-27 19:31:00 +0530

    .

--- routes/userAuth.js
+++ routes/userAuth.js
@@ -5,14 +5,17 @@ function userAuth(token) {
 		return null
 	}
 
+	console.log('userauth: ' + token)
 	User.findOne({token}, (err, doc) => {
 		if (err) {
 			throw new Error('Internal server error')
 		}
 
 		if(doc) {
+			console.log('found' + doc)
 			return doc.username
 		} else {
+			console.log('not found')
 			return null
 		}
 
